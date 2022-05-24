1f98c24d9bd87000e238f0e1fe9cf8bdb0605f77
soheil <soheilrasekh555@gmail.com>
2022-05-24 12:37:39 +0430

    configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from '@rollup/plugin-typescript';
+import dts from "rollup-plugin-dts";
+const config = [
+  {
+    input: 'build/compiled/index.js',
+    output: {
+      file: 'dist/kwildb-query-builder.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    external: ['kwildb'],
+    plugins: [typescript()]
+  }, {
+    input: 'build/compiled/index.d.ts',
+    output: {
+      file: 'dist/kwildb-query-builder.d.ts',
+      format: 'es'
+    },
+    plugins: [dts()]
+  }
+];
+export default config;
